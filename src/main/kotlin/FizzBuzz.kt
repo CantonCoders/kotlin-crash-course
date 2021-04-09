66bebfbead0b2cd59207e118edf412f9eb9a9f5c
Nathan Carver <nathan@codecarver.io>
2021-04-08 21:01:59 -0400

    Fizz using the when statement

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,14 +1,12 @@
 class FizzBuzz {
 
     fun go(number: Int): String {
-        var result = ""
-
-        if (number % 3 == 0) result += "fizz"
-        if (number % 5 == 0) result += "buzz"
-
-        if (result == "") return "${number}"
-
-        return result
+        return when {
+            number % 15 == 0 -> "fizzbuzz"
+            number % 3 == 0 -> "fizz"
+            number % 5 == 0 -> "buzz"
+            else ->  "${number}"
+        }
     }
 
 }

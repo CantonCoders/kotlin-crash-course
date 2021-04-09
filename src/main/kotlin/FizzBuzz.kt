ccd5adb1734c71f4a58b5ee8edaa65af3676e7ca
Nathan Carver <nathan@codecarver.io>
2021-04-08 20:39:06 -0400

    Input of 5 should return 'buzz'

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -2,6 +2,7 @@ class FizzBuzz {
 
     fun go(number: Int): String {
         if (number == 3) return "fizz"
+        if (number == 5) return "buzz"
 
         return "1"
     }
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,20 +1,28 @@
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class FizzBuzzTest {
 
+    private val fizzBuzz = FizzBuzz()
+
     @Test
     fun `Input of 1 should return "1"`() {
-        val fizzBuzz = FizzBuzz()
         val result = fizzBuzz.go(1)
         assertEquals("1", result)
     }
 
     @Test
     fun `Input of 3 should return "fizz"`() {
-        val fizzBuzz = FizzBuzz()
         val result = fizzBuzz.go(3)
         assertEquals("fizz", result)
+    }
+
+    @Test
+    fun `Input of 5 should return "buzz"`() {
+        val result = fizzBuzz.go(5)
+        assertEquals("buzz", result)
     }
 }

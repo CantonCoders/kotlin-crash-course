eee8145a0fe011bd8b338ee42f4ac4236d47a69f
Matthew Claysen <mclaysen@outlook.com>
2021-04-29 21:15:16 -0400

    Bowling game kata start

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -1,9 +1,55 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class BowlingTest {
 
+    private var bowlingGame = BowlingGame()
     @Test
-    fun `test`() {
+    fun `Setup inital score bowling game`() {
+        var score = bowlingGame.getScore()
 
+        assertEquals("", score)
+    }
+
+    @Test
+    fun `Player knocks down one pin`() {
+        bowlingGame.pinsKnockedDown(1)
+        var score = bowlingGame.getScore()
+
+        assertEquals("1", score)
+    }
+
+    @Test
+    fun `Player knocks down two pins`(){
+        bowlingGame.pinsKnockedDown(2)
+        var score = bowlingGame.getScore()
+
+        assertEquals("2", score)
+    }
+    @Test
+    fun `Player on second roll knocks down 2 pins over 2 rolls`(){
+        bowlingGame.pinsKnockedDown(1)
+        bowlingGame.pinsKnockedDown(1)
+
+        var score = bowlingGame.getScore()
+
+        assertEquals("11", score)
+    }
+    @Test
+    fun `Player rolls a strike`(){
+        bowlingGame.pinsKnockedDown(10)
+
+        var score = bowlingGame.getScore()
+
+        assertEquals("X", score)
+    }
+    @Test
+    fun `Player rolls a spare`(){
+        bowlingGame.pinsKnockedDown(7)
+        bowlingGame.pinsKnockedDown(3)
+
+        var score = bowlingGame.getScore()
+
+        assertEquals("7/", score)
     }
 }
